e8ea36da634b86db389ffe2ac4753d716376c62c
maksim.anatolev <maksim.anatolev@fstravel.com>
2025-07-19 17:28:31 +0300

    task-1.22 move markup to vue-components

---
+++ frontend/src/layouts/AppHeader.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+    <header class="header">
+        <div class="header__logo">
+            <a href="#" class="logo">
+                <img
+                    src="/src/assets/img/logo.svg"
+                    width="90"
+                    height="40"
+                    alt="V!U!E! Pizza logo"
+                >
+            </a>
+        </div>
+        <div class="header__cart">
+            <a href="#">0 ₽</a>
+        </div>
+        <div class="header__user">
+            <a
+                href="#"
+                class="header__login"
+            ><span>Войти</span></a>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppHeader from '@/layouts/AppHeader.vue';
+
+</script>
+
+<template>
+    <app-header/>
+    <slot/>
+</template>
+
+<style scoped>
+</style>
