83cdbff5d9efd68abcf2ef0aab6e0c1dcbecd2af
maksim.anatolev <maksim.anatolev@fstravel.com>
2025-07-19 20:38:30 +0300

    fix import app-header

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AppHeader from '@/layouts/AppHeader.vue';
+import {AppHeader} from '@/layouts';
 
 </script>
 
--- frontend/src/layouts/index.js
+++ frontend/src/layouts/index.js
@@ -1,2 +1,2 @@
-export { default as AppLayout } from "./AppLayout.vue";
-export { default as AppHeader } from "./AppHeader.vue";
+export { default as AppLayout } from './AppLayout.vue';
+export { default as AppHeader } from './AppHeader.vue';
